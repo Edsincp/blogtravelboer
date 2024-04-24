331648148512c5d43f9e4b1e5e62b714b2b4e867
edsinn <edsonpereiracarvalho10@gmail.com>
2024-04-24 14:03:50 -0300

    Atividade em andamento

--- blogtravel/blogtravel/src/components/Footer.js
+++ blogtravel/blogtravel/src/components/Footer.js
@@ -2,8 +2,9 @@ import React from "react";
 import styles from "./Footer.module.css"; 
 
 const Footer = () => { 
+    
     return(
-        <footer className={styles.footer}>
+        <footer className={styles.Footer}>
             <h6>Desenvolvido por:</h6>
             <h6>Blog travels &copy; 2024</h6>
         </footer>   
